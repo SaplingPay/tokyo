ab3b725db6fd977f881924855498051686d6dcda
chris-sapling <chris@saplingpay.com>
2024-04-16 14:10:42 +0200

    sync

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,55 +7,23 @@ import { savedStore } from './store/state'
 
 import SavedDrawer from "@/components/savedDrawer";
 import RestaurantViewDrawer from "@/components/restaurantViewDrawer";
-<<<<<<< HEAD
-import RecommendedDrawer from "@/components/recommendedDrawer";
-=======
 import { Modal } from "antd";
 import { CreateMenu, GetMenus, GetVenues, UpdateVenue } from "./actions";
 import SignInModal from "@/components/signInModal";
 import { useUser } from "@clerk/nextjs";
 import SignInPage from "@/components/signIn";
->>>>>>> 3f2fdae (Add User Auth)
+import RecommendedDrawer from "@/components/recommendedDrawer";
 
 
 export default function Home() {
 
-<<<<<<< HEAD
-=======
-  const [openVenueDrawer, setOpenVenueDrawer] = useState(false)
-  const [selectedVenue, setSelectedVenue] = useState<any>(null)
-  const [currentTab, setCurrentTab] = useState("For You")
-  const [openSignInModal, setOpenSignInModal] = useState(false)
-
-  const { user } = useUser();
-
->>>>>>> 3f2fdae (Add User Auth)
   useEffect(() => {
     savedStore.persist.rehydrate()
   }, [])
 
-<<<<<<< HEAD
-=======
-  useEffect(() => {
-    setOpenVenueDrawer(!!selectedVenue)
-  }, [selectedVenue])
-
-  useEffect(() => {
-    if (currentTab === "Saved") {
-      setOpenSignInModal(true)
-    }
-  }, [currentTab])
-
-
-  // if (!user) {
-  //   return <SignInPage />
-  // }
-
->>>>>>> 3f2fdae (Add User Auth)
   return (
     <main>
 
-<<<<<<< HEAD
       <Map />
       <Navbar />
       <ProfileIcon />
@@ -63,13 +31,8 @@ export default function Home() {
       <RestaurantViewDrawer />
       <RecommendedDrawer />
       <SavedDrawer />
-=======
-      <Navbar currentTab={currentTab} setCurrentTab={setCurrentTab} />
 
-      <RestaurantViewDrawer setOpen={setOpenVenueDrawer} open={openVenueDrawer} selectedVenue={selectedVenue} />
->>>>>>> 3f2fdae (Add User Auth)
-
-      <SignInModal openModal={openSignInModal} setOpenModal={setOpenSignInModal} />
+      {/* <SignInModal openModal={openSignInModal} setOpenModal={setOpenSignInModal} /> */}
 
     </main>
   );
